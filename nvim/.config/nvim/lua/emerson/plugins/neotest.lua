36b5f36886a7c3945f74f6df3bc357d358441b66
Emerson Barros <emersonfb99@gmail.com>
2024-05-04 14:43:14 -0300

    refactor(neotest): adds keymaps

--- nvim/.config/nvim/lua/emerson/plugins/neotest.lua
+++ nvim/.config/nvim/lua/emerson/plugins/neotest.lua
@@ -8,8 +8,15 @@ return {
 
     -- Adapters
     'nvim-neotest/neotest-go',
+    'nvim-neotest/neotest-jest',
   },
   cmd = 'Neotest',
+  keys = {
+    {'<leader>tt', '<cmd>Neotest summary toggle<CR>', desc = 'Neo[T]est [T]oggle'},
+    {'<leader>tp', '<cmd>Neotest output-panel<CR>', desc = 'Neo[T]est [P]anel'},
+    {'<leader>tf', '<cmd>Neotest run<CR>', desc = 'Neo[T]est [f]unction'},
+    {'<leader>tF', '<cmd>Neotest run file<CR>', desc = 'Neo[T]est [F]ile'},
+  },
   config = function()
     -- get neotest namespace (api call creates or returns namespace)
     local neotest_ns = vim.api.nvim_create_namespace 'neotest'
