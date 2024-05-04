065bc3f824bdb029d2dc25258451088da4738730
Emerson Barros <emersonfb99@gmail.com>
2024-05-04 14:51:10 -0300

    chore(misc): removes borders configs

--- nvim/.config/nvim/lua/emerson/core/misc.lua
+++ nvim/.config/nvim/lua/emerson/core/misc.lua
@@ -56,18 +56,3 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
     vim.opt_local.conceallevel = 0
   end,
 })
-
--- [[ Borders around windows ]]
-local _border = 'single'
-
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = _border,
-})
-
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = _border,
-})
-
-vim.diagnostic.config {
-  float = { border = _border },
-}
