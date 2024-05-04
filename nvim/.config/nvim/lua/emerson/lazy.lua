3b338781620fc40c8f7a8f339123b3cd110fe932
Emerson Barros <emersonfb99@gmail.com>
2024-05-04 14:36:46 -0300

    chore(lazy): improves indenting

--- nvim/.config/nvim/lua/emerson/lazy.lua
+++ nvim/.config/nvim/lua/emerson/lazy.lua
@@ -11,7 +11,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({ { import = 'emerson.plugins' }, { import = 'emerson.plugins.languages' } }, {
+require('lazy').setup({
+  { import = 'emerson.plugins' },
+  { import = 'emerson.plugins.languages' },
+}, {
   performance = {
     cache = {
       enabled = true,
