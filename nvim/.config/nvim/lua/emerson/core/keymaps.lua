b05e2179ac77b0c2389c86f0533d661a78d487b5
Emerson Barros <emersonfb99@gmail.com>
2024-05-04 14:09:41 -0300

    chore(keymaps): removes commented netrw keymap

--- nvim/.config/nvim/lua/emerson/core/keymaps.lua
+++ nvim/.config/nvim/lua/emerson/core/keymaps.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = ' '
 
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
--- vim.keymap.set('n', '<leader>fe', ':25Lex<CR>', { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
