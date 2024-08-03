cc6e79b64821cd3d15c0e5579a012889427d7230
Emerson Barros <emersonfb99@gmail.com>
2024-08-03 15:49:38 -0300

    config(nvim/which-key): updates version and spec

--- nvim/.config/nvim/lua/emerson/plugins/which-key.lua
+++ nvim/.config/nvim/lua/emerson/plugins/which-key.lua
@@ -3,28 +3,30 @@ return {
   event = 'VeryLazy',
   opts = {
     plugins = { spelling = true },
-    defaults = {
-      mode = { 'n', 'v' },
-      ['g'] = { name = '+goto' },
-      ['gs'] = { name = '+surround' },
-      [']'] = { name = '+next' },
-      ['['] = { name = '+prev' },
-      ['<leader>c'] = { name = '+[C]ode' },
-      ['<leader>cc'] = { name = '+[C]opilotChat' },
-      ['<leader>d'] = { name = '+[D]ebug' },
-      ['<leader>e'] = { name = '+[E]xplorer' },
-      ['<leader>f'] = { name = '+[F]ind' },
-      ['<leader>g'] = { name = '+[G]it' },
-      ['<leader>gh'] = { name = '+[H]unks' },
-      ['<leader>G'] = { name = '+[G]o' },
-      ['<leader>h'] = { name = '+[H]arpoon' },
-      ['<leader>s'] = { name = '+[S]earch' },
-      ['<leader>t'] = { name = '+Neo[T]est' },
+    defaults = {},
+    spec = {
+      {
+        mode = { 'n', 'v' },
+        { '<leader>G', group = '[G]o' },
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>cc', group = '[C]opilot[C]hat' },
+        { '<leader>d', group = '[D]ebug' },
+        { '<leader>e', group = '[E]xplorer' },
+        { '<leader>f', group = '[F]ind' },
+        { '<leader>g', group = '[G]it' },
+        { '<leader>gh', group = '[H]unks' },
+        { '<leader>h', group = '[H]arpoon' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>t', group = 'Neo[T]est' },
+        { '[', group = 'prev' },
+        { ']', group = 'next' },
+        { 'g', group = 'goto' },
+        { 'gs', group = 'surround' },
+      },
     },
   },
   config = function(_, opts)
     local wk = require 'which-key'
     wk.setup(opts)
-    wk.register(opts.defaults)
   end,
 }
