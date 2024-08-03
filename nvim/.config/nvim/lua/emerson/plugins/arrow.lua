c29bed66d77e435a67628e15ed6c38e239f84424
Emerson Barros <emersonfb99@gmail.com>
2024-08-03 15:59:21 -0300

    config(nvim/arrow): adds new plugin

---
+++ nvim/.config/nvim/lua/emerson/plugins/arrow.lua
@@ -0,0 +1,14 @@
+return {
+  'otavioschwanck/arrow.nvim',
+  keys = {
+    '<leader>;',
+    mode = { 'n', 'v' },
+    '<cdm>Arrow open<CR>',
+  },
+  opts = {
+    show_icons = true,
+    leader_key = '<leader>;',
+    buffer_leader_key = 'm', -- Per Buffer Mappings
+    separate_by_branch = true,
+  },
+}
