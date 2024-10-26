14f12a31bede8d6fcc1331d3865a198e0b8b8f78
Emerson Barros <emersonfb99@gmail.com>
2024-10-26 15:23:11 -0300

    feat(dap): enhance nvim-dap configuration and key bindings
    - Added `nvim-dap-virtual-text` plugin for inline variable display.
    - Updated key bindings for better debugging workflow.
    - Configured `dapui` to open and close automatically on debug events.
    - Enabled loading of `.vscode/launch.json` for debug configurations.

--- nvim/.config/nvim/lua/emerson/plugins/dap.lua
+++ nvim/.config/nvim/lua/emerson/plugins/dap.lua
@@ -1,30 +1,109 @@
 return {
   'mfussenegger/nvim-dap',
   dependencies = {
-    { 'rcarriga/nvim-dap-ui', dependencies = 'nvim-neotest/nvim-nio' },
+    'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio',
+    'theHamsta/nvim-dap-virtual-text',
     'leoluz/nvim-dap-go',
   },
   lazy = true,
   enabled = true,
   keys = {
-    { '<leader>db', "<cmd>lua require('dap').toggle_breakpoint()<CR>", desc = '[D]ebug: Toggle [B]reakpoint' },
-    { '<leader>du', "<cmd>lua require('dapui').toggle()<CR>", desc = '[D]ebug: Toggle UI' },
-    { '<leader>ds', "<cmd>lua require('dap').continue()<CR>", desc = '[D]ebug: [S]tart/Continue' },
-    { '<leader>di', "<cmd>lua require('dap').step_into()<CR>", desc = '[D]ebug: Step [I]nto' },
-    { '<leader>do', "<cmd>lua require('dap').step_over()<CR>", desc = '[D]ebug: Step [O]ver' },
-    { '<leader>de', "<cmd>lua require('dap').step_out()<CR>", desc = '[D]ebug: Step Out/[E]xit' },
+    {
+      '<leader>db',
+      function()
+        require('dap').toggle_breakpoint()
+      end,
+      desc = 'toggle [D]ebug [b]reakpoint',
+    },
+    {
+      '<leader>dc',
+      function()
+        require('dap').continue()
+      end,
+      desc = '[D]ebug [c]ontinue (start here)',
+    },
+    {
+      '<leader>dC',
+      function()
+        require('dap').run_to_cursor()
+      end,
+      desc = '[D]ebug [C]ursor',
+    },
+    {
+      '<leader>do',
+      function()
+        require('dap').step_over()
+      end,
+      desc = '[D]ebug step [o]ver',
+    },
+    {
+      '<leader>dO',
+      function()
+        require('dap').step_out()
+      end,
+      desc = '[D]ebug step [O]ut',
+    },
+    {
+      '<leader>di',
+      function()
+        require('dap').step_into()
+      end,
+      desc = '[D]ebug [i]nto',
+    },
+    {
+      '<leader>dR',
+      function()
+        require('dap').clear_breakpoints()
+      end,
+      desc = '[D]ebug [R]emove breakpoints',
+    },
+    {
+      '<leader>dt',
+      function()
+        require('dap').terminate()
+      end,
+      desc = '[D]ebug [t]erminate',
+    },
+    {
+      '<leader>dw',
+      function()
+        require('dap.ui.widgets').hover()
+      end,
+      desc = '[D]ebug [w]idgets',
+    },
+    {
+      '<leader>du',
+      function()
+        require('dapui').toggle()
+      end,
+      desc = '[D]ebug: Toggle [U]I',
+    },
   },
   config = function()
     local dap = require 'dap'
+    local ui = require 'dapui'
 
-    local dapui = require 'dapui'
-    dapui.setup {}
+    require('nvim-dap-virtual-text').setup {}
+    require('dapui').setup()
+    -- setup dap config by .vscode/launch.json file
+    require('dap.ext.vscode').load_launchjs()
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['daui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+    dap.listeners.before.attach.dapui_config = function()
+      ui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      ui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      ui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      ui.close()
+    end
 
-    local filetype = vim.api.nvim_buf_get_option(0, 'filetype')
+    -- vim.api.nvim_get_option_value('filetype')
+    local filetype = vim.api.nvim_get_option_value('filetype', {})
     if filetype == 'javascript' or filetype == 'typescript' then
       -- js/ts hell
       local function get_js_debug()
