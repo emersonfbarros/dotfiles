efeb4d08bb7fa5b035dc2b0d65574bd6a4406d4c
Emerson Barros <emersonfb99@gmail.com>
2024-10-26 15:22:14 -0300

    feat(gitsigns): add custom signs for staged and unstaged changes
    - Added custom signs for add, change, delete, topdelete, changedelete, and untracked changes.
    - Introduced `signs_staged` configuration for staged changes.

--- nvim/.config/nvim/lua/emerson/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/emerson/plugins/gitsigns.lua
@@ -2,6 +2,21 @@ return {
   'lewis6991/gitsigns.nvim',
   event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
   opts = {
+    signs = {
+      add = { text = '▎' },
+      change = { text = '▎' },
+      delete = { text = '' },
+      topdelete = { text = '' },
+      changedelete = { text = '▎' },
+      untracked = { text = '▎' },
+    },
+    signs_staged = {
+      add = { text = '▎' },
+      change = { text = '▎' },
+      delete = { text = '' },
+      topdelete = { text = '' },
+      changedelete = { text = '▎' },
+    },
     attach_to_untracked = true,
     on_attach = function(bufnr)
       local gs = package.loaded.gitsigns
