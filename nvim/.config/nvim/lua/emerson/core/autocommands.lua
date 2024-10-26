988068daa52e2836b2f7329cefc2d78bac3e0a7e
Emerson Barros <emersonfb99@gmail.com>
2024-10-26 15:12:20 -0300

    refactor(autocommands): simplify autocmd creation and improve terminal settings
    - Replaced table syntax with single string for autocmd events.
    - Updated `VimResized` and `FileType` autocmds to use consistent syntax.
    - Added new autocmd for `TermOpen` to set terminal-specific options.
    - Improved readability by aligning patterns and removing extra spaces.

--- nvim/.config/nvim/lua/emerson/core/autocommands.lua
+++ nvim/.config/nvim/lua/emerson/core/autocommands.lua
@@ -11,7 +11,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- resize splits if window got resized
-vim.api.nvim_create_autocmd({ 'VimResized' }, {
+vim.api.nvim_create_autocmd('VimResized', {
   group = augroup 'resize_splits',
   callback = function()
     local current_tab = vim.fn.tabpagenr()
@@ -49,7 +49,7 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 -- Fix conceallevel for json files
-vim.api.nvim_create_autocmd({ 'FileType' }, {
+vim.api.nvim_create_autocmd('FileType', {
   group = augroup 'json_conceal',
   pattern = { 'json', 'jsonc', 'json5' },
   callback = function()
@@ -57,21 +57,32 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
   end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("close_with_q"),
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup 'close_with_q',
   pattern = {
-    "help",
-    "lspinfo",
-    "notify",
-    "qf",
-    "neotest-output",
-    "checkhealth",
-    "neotest-summary",
-    "neotest-output-panel",
-    "dbout",
+    'help',
+    'lspinfo',
+    'notify',
+    'qf',
+    'neotest-output',
+    'checkhealth',
+    'neotest-summary',
+    'neotest-output-panel',
+    'dbout',
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+    vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = augroup 'custom_term_open',
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.scrolloff = 0
+
+    vim.bo.filetype = 'terminal'
   end,
 })
